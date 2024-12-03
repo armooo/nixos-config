c7e6c0d9596f62fad6f8bd662b14a6fe3ed545a6
Jason Michalski <armooo@armooo.net>
2024-12-02 18:18:20 -0800

    Add real craptop config.

--- nodes/craptop/default.nix
+++ nodes/craptop/default.nix
@@ -1,8 +1,13 @@
 { nixos-hardware, ... }:
 {
   imports = [
+    ./hardware-configuration.nix
     nixos-hardware.nixosModules.apple-macbook-pro-11-1
-  ]
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "craptop";
 
