1a7df5ba3f5c77e27bd291ce196444712a649126
Jason Michalski <armooo@armooo.new>
2024-12-04 19:44:26 -0800

    bluetooth audio

--- desktop-env/default.nix
+++ desktop-env/default.nix
@@ -41,6 +41,8 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  hardware.bluetooth.enable = true;
+
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
--- users/armooo/home/default.nix
+++ users/armooo/home/default.nix
@@ -24,5 +24,6 @@
     iotop
     mplayer
     vlc
+    pavucontrol
   ];
 }
