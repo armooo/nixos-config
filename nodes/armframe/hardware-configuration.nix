4778b0b80f4067f6a369b7814b0eacc631f5db9c
Jason Michalski <armooo@armooo.new>
2024-12-14 18:24:01 -0800

    mount options

--- nodes/armframe/hardware-configuration.nix
+++ nodes/armframe/hardware-configuration.nix
@@ -6,32 +6,36 @@
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/5afcc8f3-3f41-4642-945c-3d7c0695988b";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/5afcc8f3-3f41-4642-945c-3d7c0695988b";
+    fsType = "ext4";
+    options = [ "discard" "data=writeback" "journal_async_commit" "auto_da_alloc"];
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D919-290E";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/D919-290E";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/4776bed3-0455-4366-aa41-881bbb3b6650";
-      fsType = "ext4";
-    };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/4776bed3-0455-4366-aa41-881bbb3b6650";
+    fsType = "ext4";
+    options = [ "discard" "data=writeback" "journal_async_commit" "auto_da_alloc"];
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/269df0d1-9c94-4ebc-b6a8-ca563d493b08"; }
-    ];
+  swapDevices = [
+    {
+      device = "/dev/disk/by-uuid/269df0d1-9c94-4ebc-b6a8-ca563d493b08";
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
