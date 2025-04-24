674897ddac060ed2695a5689860e629f8ffa6a00
Jason Michalski <armooo@armooo.net>
2025-04-23 19:58:26 -0700

    Update

--- common/default.nix
+++ common/default.nix
@@ -25,7 +25,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 7d";
+    options = "--delete-older-than 30d";
   };
   nix.optimise.automatic = true;
   nix.extraOptions =  ''
