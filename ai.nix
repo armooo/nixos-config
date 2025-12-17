c810d1d8f028a7341e1fdb0a6cd6e2fe0cb39e60
Jason Michalski <armooo@armooo.net>
2025-12-16 16:33:48 -0800

    stop pulling models

--- ai.nix
+++ ai.nix
@@ -3,10 +3,6 @@
   services.ollama = {
     enable = true;
     acceleration = "vulkan";
-    loadModels = [
-      "gpt-oss:20b"
-      "deepseek-r1:8b"
-    ];
     environmentVariables = {
       OLLAMA_FLASH_ATTENTION = "1";
       OLLAMA_CONTEXT_LENGTH = "128000";
