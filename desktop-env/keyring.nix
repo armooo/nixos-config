e6b57d8c059b8540045d5e0aa8a723127716be82
Jason Michalski <armooo@armooo.net>
2026-02-25 23:02:49 -0800

    more keyring

--- desktop-env/keyring.nix
+++ desktop-env/keyring.nix
@@ -4,5 +4,8 @@
 {
   services.gnome.gnome-keyring.enable = true;
   services.gnome.gcr-ssh-agent.enable = true;
-  security.pam.services.gdm.enableGnomeKeyring = true;
+  security.pam.services = {
+    gdm.enableGnomeKeyring = true;
+    login.enableGnomeKeyring = true;
+  };
 }
