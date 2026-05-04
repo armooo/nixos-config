599c56410cde02a1d7d1e18815c65c18f5dc4a4d
Jason Michalski <armooo@armooo.net>
2026-05-04 15:25:52 -0700

    update recovery pass

--- nodes/armframe/disk_encryption.nix
+++ nodes/armframe/disk_encryption.nix
@@ -9,7 +9,7 @@
     pkgs.fscrypt-experimental
   ];
 
-  boot.initrd.systemd.emergencyAccess = "$7$GU..../....V3ru3yY4SwEsGUdbCZqa6.$jz9GFRf3MREZuNKqd286/7gLyIzNI/1fUAAihZFvWtD";
+  boot.initrd.systemd.emergencyAccess = "$7$GU..../....bTbQZYsqcj45Zhkl2q9OH.$RagcUFlC0P3gikVxL9xjHFtm9hb8EnM.h6B4vZPysl6";
   boot.initrd.systemd.enable = true; # for tpm2 unlock
   boot.initrd.luks.devices.swap = {
     device = "/dev/disk/by-uuid/a8f2ce7e-fc22-46dd-879b-6a4b0833af1f";
